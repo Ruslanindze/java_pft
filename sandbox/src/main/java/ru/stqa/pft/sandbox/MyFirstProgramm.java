629886120bc846051b9bc682731f7cd4d187031f
Ruslanindze <42635477+Ruslanindze@users.noreply.github.com>
2021-08-15 11:25:47 +0300

    Функции перенесены в классы, чтобы стать методами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -8,22 +8,13 @@ public class MyFirstProgramm {
         hello("Hello Rus!");
 
         Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадь прямоугольника со сторонома " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("Площадь прямоугольника со сторонома " + r.a + " и " + r.b + " = " + s.area());
     }
 
     public static void hello(String somebody) {
         System.out.println(somebody);
-    }
-
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
-
-    // Перегрузка функции по кол-ву параметров
-    public static double area(Rectangle r) {
-        return r.a * r.b;
     }
 }
