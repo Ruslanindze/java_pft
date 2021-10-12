5a72ff6586453d147c91b8a4532cc0d2385c2df9
Ruslanindze <42635477+Ruslanindze@users.noreply.github.com>
2021-10-12 15:33:16 +0300

    Рефакторинг пробного теста

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
@@ -20,59 +20,75 @@ public class GroupCreationTests {
         System.setProperty("webdriver.gecko.driver", ConfProperties.getProperty("firefoxDriver"));
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperty("loginPage"));
+        login();
     }
 
-    @Test
-    public void groupCreationTests() {
-        // STEP 1. Открываем сайт
-        driver.get(ConfProperties.getProperty("loginPage"));
-
-        // STEP 2. Логинимся
+    private void login() {
         WebElement loginName = driver.findElement(By.xpath("//form[@id='LoginForm']/input[@name='user']"));
         loginName.clear();
         loginName.sendKeys(ConfProperties.getProperty("login"));
-
         WebElement loginPassword = driver.findElement(By.xpath("//form[@id='LoginForm']/input[@name='pass']"));
         loginPassword.clear();
         loginPassword.sendKeys(ConfProperties.getProperty("password"));
-
         WebElement buttonInput = driver.findElement(By.xpath("//form[@id='LoginForm']/input[@value='Login']"));
         buttonInput.click();
 
-        // STEP 3. Проверяем пользователя.
         String nameUser = driver.findElement(By.xpath("//form[@name='logout']/b")).getText();
         Assert.assertTrue(nameUser.contains(ConfProperties.getProperty("login")), "Логин прошёл успешно.");
+    }
 
-        // STEP 4. Переходим в меню groups через нажатие кнопки и проверяем url.
+    @Test
+    public void testGroupCreation() {
+        gotoGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("June Sopranos", "The Sopranos rule!", "This is our kitchen. Watch out for the Soparetto family !!!"));
+        submitGroupCreation();
+        returnToGroupPage("June Sopranos");
+    }
+
+    private void gotoGroupPage() {
         WebElement buttonGroups = driver.findElement(By.xpath("//div[@id='nav']/ul/li[@class='admin']"));
         buttonGroups.click();
         Assert.assertEquals(driver.getCurrentUrl(), ConfProperties.getProperty("groupsPage"));
+    }
 
-        // STEP 5. Создаём группу с произвольными данными.
+    private void initGroupCreation() {
         WebElement buttonNewGroup = driver.findElement(By.xpath("//input[@name='new'][1]"));
         buttonNewGroup.click();
+    }
 
+    private void fillGroupForm(GroupData groupData) {
         WebElement fieldGroupName = driver.findElement(By.xpath("//input[@name='group_name']"));
         fieldGroupName.click();
-        fieldGroupName.sendKeys("Джуны Сопрано");
+        fieldGroupName.sendKeys(groupData.getName());
 
         WebElement fieldGroupHeader = driver.findElement(By.xpath("//textarea[@name='group_header']"));
-        fieldGroupHeader.sendKeys("Сопрано рулят!");
+        fieldGroupHeader.sendKeys(groupData.getHeader());
 
         WebElement fieldFooter = driver.findElement(By.xpath("//textarea[@name='group_footer']"));
-        fieldFooter.sendKeys("Тут наша кухня. Берегитесь семьи Сопаретто!!!");
+        fieldFooter.sendKeys(groupData.getFooter());
+    }
 
+    private void submitGroupCreation() {
         WebElement buttonEnterInfo = driver.findElement(By.xpath("//input[@name='submit']"));
         buttonEnterInfo.click();
+    }
 
-        // STEP 6. Возвращаемся в список групп и проверяем созданную группу.
+    private void returnToGroupPage(String nameGroup) {
         WebElement linkReturnGroupPage = driver.findElement(By.xpath("//div[@class='msgbox']//a"));
         linkReturnGroupPage.click();
         ArrayList<WebElement> groups = (ArrayList<WebElement>) driver.findElements(By.xpath("//div[@id='content']//span"));
 
-//        for (WebElement group: groups) {
-//
-//        }
+        boolean findNeedGroup = false;
+        for (WebElement group : groups) {
+            String currName = group.getText();
+            if (nameGroup.equals(currName)) {
+                findNeedGroup = true;
+                break;
+            }
+        }
+        Assert.assertTrue(findNeedGroup);
     }
 
     @AfterMethod
